6efc6eb5ce57b7c7efea780aa762916b03a6e0e7
jmagrinho <jmb.magrinho@hotmail.com>
2022-09-11 20:44:43 +0100

    Update index.test.ts
    I'm not sure if these are the tests i need to do. I want to test the public functions on my controller, but based on the example given i did these.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,48 +1,48 @@
 import { request } from './utils';
 
-//#region Tests Endpoint 1
+#region Tests Endpoint 1
 
-// describe('Endpoint 1 - GET ', () => {
-//   test('Should warn page number is invalid (if it is < 0 or or not number)', async () => {
-//     const result = await request.get('/ANY_NAME/-1');
-//     const resultJSON = JSON.parse(result.text)
-//     expect(resultJSON.status_code).toBe(400);
-//     expect(resultJSON.status_message).toBe("Page number is invalid.");
-//   }),
+describe('Endpoint 1 - GET ', () => {
+  test('Should warn page number is invalid (if it is < 0 or or not number)', async () => {
+    const result = await request.get('/ANY_NAME/-1');
+    const resultJSON = JSON.parse(result.text)
+    expect(resultJSON.status_code).toBe(400);
+    expect(resultJSON.status_message).toBe("Page number is invalid.");
+  }),
 
-//   test('Should warn page number is invalid V2 (if it is < 0 or not number)', async () => {
-//     const result = await request.get('/ANY_NAME/a');
-//     const resultJSON = JSON.parse(result.text)
-//     expect(resultJSON.status_code).toBe(400);
-//     expect(resultJSON.status_message).toBe("Page number is invalid.");
-//   }),
+  test('Should warn page number is invalid V2 (if it is < 0 or not number)', async () => {
+    const result = await request.get('/ANY_NAME/a');
+    const resultJSON = JSON.parse(result.text)
+    expect(resultJSON.status_code).toBe(400);
+    expect(resultJSON.status_message).toBe("Page number is invalid.");
+  }),
 
-//   test('Result status should be OK even if the organisation name does not exist', async () => {
-//     const result = await request.get('/ANY_NON_EXISTING_NAME/0');
-//     const resultJSON = JSON.parse(result.text)
-//     expect(resultJSON.status_code).toBe(200);
-//     expect(resultJSON.status_message).toBe("OK");
-//   }),
+  test('Result status should be OK even if the organisation name does not exist', async () => {
+    const result = await request.get('/ANY_NON_EXISTING_NAME/0');
+    const resultJSON = JSON.parse(result.text)
+    expect(resultJSON.status_code).toBe(200);
+    expect(resultJSON.status_message).toBe("OK");
+  }),
 
-//   test('An existing organisation name should bring results on first page', async () => {
-//     const result = await request.get('/Black%20Banana/0');
-//     const resultJSON = JSON.parse(result.text)
-//     expect(resultJSON.status_code).toBe(200);
-//     expect(resultJSON.status_message).toBe("OK");
-//     expect(resultJSON.results_count).toBeGreaterThan(0);
-//   }),
+  test('An existing organisation name should bring results on first page', async () => {
+    const result = await request.get('/Black%20Banana/0');
+    const resultJSON = JSON.parse(result.text)
+    expect(resultJSON.status_code).toBe(200);
+    expect(resultJSON.status_message).toBe("OK");
+    expect(resultJSON.results_count).toBeGreaterThan(0);
+  }),
 
-//   test('README Example - Black Banana organisation results:', async () => {
-//     const result = await request.get('/Black%20Banana/0');
-//     const resultJSON = JSON.parse(result.text)
-//     expect(resultJSON.status_code).toBe(200);
-//     expect(resultJSON.status_message).toBe("OK");
-//     expect(JSON.stringify(resultJSON.results)).toBe(`[{"org_name":"Banana tree","relationship_type":"parent"},{"org_name":"Big banana tree","relationship_type":"parent"},{"org_name":"Brown Banana","relationship_type":"sister"},{"org_name":"Green Banana","relationship_type":"sister"},{"org_name":"Phoneutria Spider","relationship_type":"daughter"},{"org_name":"Yellow Banana","relationship_type":"sister"}]`)
+  test('README Example - Black Banana organisation results:', async () => {
+    const result = await request.get('/Black%20Banana/0');
+    const resultJSON = JSON.parse(result.text)
+    expect(resultJSON.status_code).toBe(200);
+    expect(resultJSON.status_message).toBe("OK");
+    expect(JSON.stringify(resultJSON.results)).toBe(`[{"org_name":"Banana tree","relationship_type":"parent"},{"org_name":"Big banana tree","relationship_type":"parent"},{"org_name":"Brown Banana","relationship_type":"sister"},{"org_name":"Green Banana","relationship_type":"sister"},{"org_name":"Phoneutria Spider","relationship_type":"daughter"},{"org_name":"Yellow Banana","relationship_type":"sister"}]`)
     
-//   })
+  })
 
 
-// });
+});
 
 //#endregion
 
